9e806f522dd842f3926b202f9a5df83f9e3428c3
Tomohiro Akinaga <tomo@TomohironoMacBook-Pro.local>
2022-06-30 13:54:02 -0700

    added json file

---
+++ src/components/CardList/CardList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CardList: React.FC = () => {
+    return <div>CardList</div>;
+};
+
+export default CardList;
--- src/pages/Top/Top.tsx
+++ src/pages/Top/Top.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "../../components/Header/Header";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import Footer from "../../components/Footer/Footer";
+import CardList from "../../components/CardList/CardList";
 
-const Top = () => {
+const Top: React.FC = () => {
     return (
         <>
             <Header />
             <SearchBar />
+            <CardList />
             <Footer />
         </>
     );
